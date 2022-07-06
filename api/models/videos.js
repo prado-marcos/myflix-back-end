767906f331b0e331e49093df13f04f1392d5cabe
Marcos Paulo Santos Prado <marcospaulo_prado@live.com>
2022-07-06 16:03:00 -0300

    feat: add videos model

---
+++ api/models/videos.js
@@ -0,0 +1,47 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+    class Videos extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+    Videos.init(
+        {
+            titulo: {
+                type: DataTypes.STRING,
+                validate: {
+                    notEmpty: {
+                        msg: "Título não pode ser vazio",
+                    },
+                },
+            },
+            descricao: {
+                type: DataTypes.STRING,
+                validate: {
+                    notEmpty: {
+                        msg: "Descrição não pode ser vazia",
+                    },
+                },
+            },
+            url: {
+                type: DataTypes.STRING,
+                validate: {
+                    isUrl: {
+                        msg: "Formato inválido de ULR",
+                    },
+                },
+            },
+        },
+        {
+            sequelize,
+            modelName: "Videos",
+        }
+    );
+    return Videos;
+};
