88cf4543a024dcd816701837fc9a96d63abd2de3
Marcos Paulo Santos Prado <marcospaulo_prado@live.com>
2022-07-13 12:27:20 -0300

    feat: add new seeds

--- api/seeders/20220704000136-demo-videos.js
+++ api/seeders/20220704000136-demo-videos.js
@@ -6,9 +6,41 @@ module.exports = {
             "Videos",
             [
                 {
-                    titulo: "",
-                    descricao: "Teste",
-                    url: "Teste",
+                    titulo: "Bring Me The Horizon - Can You Feel My Heart",
+                    descricao:
+                        "`Can You Feel My Heart` by Bring Me The Horizon",
+                    url: "https://www.youtube.com/watch?v=QJJYpsA5tv8&ab_channel=BMTHOfficialVEVO",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    titulo: "Fabulous Secret Powers",
+                    descricao: "`HEHEYYEYAAEYAAAEYAEYAA` / `He-Man Sings`",
+                    url: "https://www.youtube.com/watch?v=FR7wOGyAzpw&list=PLHEBsC_NxJ_R3KnXJa4BfF7NEbnNvW_jg&ab_channel=slackcircus",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    titulo: "5 Hours of Super Mario Music",
+                    descricao: "5 Hours of Super Mario Music",
+                    url: "https://www.youtube.com/watch?v=KCiVG6mTor0&ab_channel=Josiaal",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    titulo: "Rick Astley - Never Gonna Give You Up (Official Music Video)",
+                    descricao:
+                        "The official video for `Never Gonna Give You Up` by Rick Astley",
+                    url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    titulo: "SiM - The Rumbling (Anime Special Ver.)",
+                    descricao: `“The Rumbling” (Anime Special Ver.)
+                    Director : Motohiro Ishii (Ray)
+                    Producer : Masako Yoshikawa (Ray)`,
+                    url: "https://www.youtube.com/watch?v=DSMuhzSgOGE&ab_channel=SiMOfficialYouTubeChannel",
                     createdAt: new Date(),
                     updatedAt: new Date(),
                 },
@@ -18,6 +50,6 @@ module.exports = {
     },
 
     async down(queryInterface, Sequelize) {
-        await queryInterface.bulkDelete("People", null, {});
+        await queryInterface.bulkDelete("Videos", null, {});
     },
 };
