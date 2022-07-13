86f17e7981ee39b190ac7ac8a5b413536c5dbfa9
Marcos Paulo Santos Prado <marcospaulo_prado@live.com>
2022-07-13 12:26:33 -0300

    refactor: separate video controller from sevices

--- api/controllers/VideoController.js
+++ api/controllers/VideoController.js
@@ -1,9 +1,13 @@
-const db = require("../models");
+const { VideosServices } = require("../services");
+const videosServices = new VideosServices();
 
 class VideoController {
     static async acessarVideos(req, res) {
         try {
-            const videos = await db.Videos.findAll();
+            const videos = await videosServices.getAll();
+            if (videos.length === 0) {
+                return res.status(200).json({ message: "Não há nenhum vídeo" });
+            }
             return res.status(200).json(videos);
         } catch (error) {
             res.status(500).json(error.message);
@@ -11,47 +15,64 @@ class VideoController {
     }
 
     static async acessarVideoPorId(req, res) {
+        const { id } = req.params;
         try {
-            const { id } = req.params;
-            const video = await db.Videos.findOne({
-                where: { id: Number(id) },
-            });
-            return res.status(200).json(video);
+            const video = await videosServices.getById(Number(id));
+            if (video) {
+                return res.status(200).json(video);
+            } else {
+                throw new Error("Vídeo não encontrado");
+            }
         } catch (error) {
-            res.status(500).json(error.message);
+            res.status(404).json(error.message);
         }
     }
 
     static async cadastarVideo(req, res) {
+        const video = req.body;
         try {
-            const video = req.body;
-            const novoVideo = await db.Videos.create(video);
-            return res.status(200).json(novoVideo);
+            const newVideo = await videosServices.create(video);
+            if (newVideo) {
+                return res.status(200).json(newVideo);
+            } else {
+                throw new Error("Não foi possível cadastar o vídeo");
+            }
         } catch (error) {
             res.status(500).json(error.message);
         }
     }
 
     static async atualizarVideo(req, res) {
+        const { id } = req.params;
+        const newData = req.body;
         try {
-            const { id } = req.params;
-            const novaInfo = req.body;
-            await db.Videos.update(novaInfo, { where: { id: Number(id) } });
-            return res
-                .status(200)
-                .json({ message: "Atualização feita com sucesso" });
+            const isVideoUpdated = await videosServices.update(
+                newData,
+                Number(id)
+            );
+            if (isVideoUpdated) {
+                return res
+                    .status(200)
+                    .json({ message: "Atualização feita com sucesso" });
+            } else {
+                throw new Error("Não foi possível atualizar o vídeo");
+            }
         } catch (error) {
             res.status(500).json(error.message);
         }
     }
 
     static async removerVideo(req, res) {
+        const { id } = req.params;
         try {
-            const { id } = req.params;
-            await db.Videos.destroy({ where: { id: Number(id) } });
-            return res
-                .status(200)
-                .json({ message: "Exclusão feita com sucesso" });
+            const isVideoRemoved = await videosServices.remove(Number(id));
+            if (isVideoRemoved) {
+                return res
+                    .status(200)
+                    .json({ message: "Exclusão feita com sucesso" });
+            } else {
+                throw new Error("Não foi possível remover o vídeo");
+            }
         } catch (error) {
             res.status(500).json(error.message);
         }
