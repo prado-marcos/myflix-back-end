7c3b0d099667ba85f2d2f518328e2db6e0aa44c5
Marcos Paulo Santos Prado <marcospaulo_prado@live.com>
2022-07-13 19:02:32 -0300

    feat: create categorias model

---
+++ api/models/categorias.js
@@ -0,0 +1,27 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+    class Categorias extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+          Categorias.hasMany(models.Videos, {
+            foreignKey: "videoId",
+          })
+        }
+    }
+    Categorias.init(
+        {
+            titulo: DataTypes.STRING,
+            cor: DataTypes.STRING,
+        },
+        {
+            sequelize,
+            modelName: "Categorias",
+        }
+    );
+    return Categorias;
+};
