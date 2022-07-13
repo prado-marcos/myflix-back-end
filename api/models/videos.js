68400c163bb72e40ae2ce1c75c751f8b8fd93458
Marcos Paulo Santos Prado <marcospaulo_prado@live.com>
2022-07-13 19:03:09 -0300

    feat: add association to videos model

--- api/models/videos.js
+++ api/models/videos.js
@@ -8,7 +8,9 @@ module.exports = (sequelize, DataTypes) => {
          * The `models/index` file will call this method automatically.
          */
         static associate(models) {
-            // define association here
+            Videos.belongsTo(models.Categorias, {
+                foreignKey: "categoriaId",
+            });
         }
     }
     Videos.init(
