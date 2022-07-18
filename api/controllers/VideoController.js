6ad556b7edb276502606a93330fcc3089fa806cc
Marcos Paulo Santos Prado <marcospaulo_prado@live.com>
2022-07-18 17:58:18 -0300

    refactor: change methods names

--- api/controllers/VideoController.js
+++ api/controllers/VideoController.js
@@ -2,9 +2,22 @@ const { VideosServices } = require("../services");
 const videosServices = new VideosServices();
 
 class VideoController {
-    static async acessarVideos(req, res) {
+    static async listarVideos(_req, res) {
         try {
-            const videos = await videosServices.getAll();
+            const videos = await videosServices.listar();
+            if (videos.length === 0) {
+                return res.status(200).json({ message: "Não há nenhum vídeo" });
+            }
+            return res.status(200).json(videos);
+        } catch (error) {
+            res.status(500).json(error.message);
+        }
+    }
+
+    static async listarVideosPorCategoria(req, res) {
+        const { id } = req.params;
+        try {
+            const videos = await videosServices.listarPorCategoria(id);
             if (videos.length === 0) {
                 return res.status(200).json({ message: "Não há nenhum vídeo" });
             }
@@ -17,7 +30,21 @@ class VideoController {
     static async acessarVideoPorId(req, res) {
         const { id } = req.params;
         try {
-            const video = await videosServices.getById(Number(id));
+            const video = await videosServices.acessarPorId(Number(id));
+            if (video) {
+                return res.status(200).json(video);
+            } else {
+                throw new Error("Vídeo não encontrado");
+            }
+        } catch (error) {
+            res.status(404).json(error.message);
+        }
+    }
+
+    static async acessarVideoPorTitulo(req, res) {
+        const { search } = req.query;
+        try {
+            const video = await videosServices.acessarPorTitulo(String(search));
             if (video) {
                 return res.status(200).json(video);
             } else {
@@ -31,9 +58,9 @@ class VideoController {
     static async cadastarVideo(req, res) {
         const video = req.body;
         try {
-            const newVideo = await videosServices.create(video);
-            if (newVideo) {
-                return res.status(200).json(newVideo);
+            const novoVideo = await videosServices.criar(video);
+            if (novoVideo) {
+                return res.status(200).json(novoVideo);
             } else {
                 throw new Error("Não foi possível cadastar o vídeo");
             }
@@ -44,13 +71,13 @@ class VideoController {
 
     static async atualizarVideo(req, res) {
         const { id } = req.params;
-        const newData = req.body;
+        const novosDados = req.body;
         try {
-            const isVideoUpdated = await videosServices.update(
-                newData,
+            const videoAtualizado = await videosServices.atualizar(
+                novosDados,
                 Number(id)
             );
-            if (isVideoUpdated) {
+            if (videoAtualizado) {
                 return res
                     .status(200)
                     .json({ message: "Atualização feita com sucesso" });
@@ -65,8 +92,8 @@ class VideoController {
     static async removerVideo(req, res) {
         const { id } = req.params;
         try {
-            const isVideoRemoved = await videosServices.remove(Number(id));
-            if (isVideoRemoved) {
+            const videoRemovido = await videosServices.remover(Number(id));
+            if (videoRemovido) {
                 return res
                     .status(200)
                     .json({ message: "Exclusão feita com sucesso" });
