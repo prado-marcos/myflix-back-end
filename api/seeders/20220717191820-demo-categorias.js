b61174a976d23441c991a3151748b6e4bd11bf8c
Marcos Paulo Santos Prado <marcospaulo_prado@live.com>
2022-07-18 18:05:30 -0300

    feat: create categorias seeds

---
+++ api/seeders/20220717191820-demo-categorias.js
@@ -0,0 +1,47 @@
+"use strict";
+
+module.exports = {
+    async up(queryInterface, _Sequelize) {
+        await queryInterface.bulkInsert(
+            "Categorias",
+            [
+                {
+                    id: 1,
+                    titulo: "LIVRE",
+                    cor: "#ffffff",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    titulo: "Meme",
+                    cor: "#bbbbbb",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    titulo: "Música",
+                    cor: "#0b0b0b",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    titulo: "Educativo",
+                    cor: "#000000",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    titulo: "Games",
+                    cor: "#dddddd",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+            ],
+            {}
+        );
+    },
+
+    async down(queryInterface, _Sequelize) {
+        await queryInterface.bulkDelete("Categorias", null, {});
+    },
+};
