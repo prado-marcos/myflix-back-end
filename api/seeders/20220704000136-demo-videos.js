2f2dd90d0d13b2ded663db2071b6d482050ca9e2
Marcos Paulo Santos Prado <marcospaulo_prado@live.com>
2022-07-18 18:05:13 -0300

    refactor: add categoriaId to the videos seeds

--- api/seeders/20220704000136-demo-videos.js
+++ api/seeders/20220704000136-demo-videos.js
@@ -1,7 +1,7 @@
 "use strict";
 
 module.exports = {
-    async up(queryInterface, Sequelize) {
+    async up(queryInterface, _Sequelize) {
         await queryInterface.bulkInsert(
             "Videos",
             [
@@ -10,6 +10,7 @@ module.exports = {
                     descricao:
                         "`Can You Feel My Heart` by Bring Me The Horizon",
                     url: "https://www.youtube.com/watch?v=QJJYpsA5tv8&ab_channel=BMTHOfficialVEVO",
+                    categoriaId: 1,
                     createdAt: new Date(),
                     updatedAt: new Date(),
                 },
@@ -17,6 +18,7 @@ module.exports = {
                     titulo: "Fabulous Secret Powers",
                     descricao: "`HEHEYYEYAAEYAAAEYAEYAA` / `He-Man Sings`",
                     url: "https://www.youtube.com/watch?v=FR7wOGyAzpw&list=PLHEBsC_NxJ_R3KnXJa4BfF7NEbnNvW_jg&ab_channel=slackcircus",
+                    categoriaId: 2,
                     createdAt: new Date(),
                     updatedAt: new Date(),
                 },
@@ -24,6 +26,7 @@ module.exports = {
                     titulo: "5 Hours of Super Mario Music",
                     descricao: "5 Hours of Super Mario Music",
                     url: "https://www.youtube.com/watch?v=KCiVG6mTor0&ab_channel=Josiaal",
+                    categoriaId: 3,
                     createdAt: new Date(),
                     updatedAt: new Date(),
                 },
@@ -32,6 +35,7 @@ module.exports = {
                     descricao:
                         "The official video for `Never Gonna Give You Up` by Rick Astley",
                     url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley",
+                    categoriaId: 4,
                     createdAt: new Date(),
                     updatedAt: new Date(),
                 },
@@ -41,6 +45,7 @@ module.exports = {
                     Director : Motohiro Ishii (Ray)
                     Producer : Masako Yoshikawa (Ray)`,
                     url: "https://www.youtube.com/watch?v=DSMuhzSgOGE&ab_channel=SiMOfficialYouTubeChannel",
+                    categoriaId: 5,
                     createdAt: new Date(),
                     updatedAt: new Date(),
                 },
@@ -49,7 +54,7 @@ module.exports = {
         );
     },
 
-    async down(queryInterface, Sequelize) {
+    async down(queryInterface, _Sequelize) {
         await queryInterface.bulkDelete("Videos", null, {});
     },
 };
