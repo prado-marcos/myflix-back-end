60c61f652c7e743d6d1efd8c286482aae87158c1
Marcos Paulo Santos Prado <marcospaulo_prado@live.com>
2022-07-18 18:06:12 -0300

    feat: add CategoriasServices to index

--- api/services/index.js
+++ api/services/index.js
@@ -1,5 +1,7 @@
 const VideosServices = require("./VideosServices.js");
+const CategoriasServices = require("./CategoriasServices.js");
 
 module.exports = {
     VideosServices,
-}
+    CategoriasServices,
+};
