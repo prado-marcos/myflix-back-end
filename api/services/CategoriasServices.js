2dff8f924286cc571eee7007835b77ea28080cf5
Marcos Paulo Santos Prado <marcospaulo_prado@live.com>
2022-07-18 18:05:46 -0300

    feat: create CategoriasServices

---
+++ api/services/CategoriasServices.js
@@ -0,0 +1,9 @@
+const Services = require("./Services.js");
+
+class CategoriasServices extends Services {
+    constructor() {
+        super("Categorias");
+    }
+}
+
+module.exports = CategoriasServices;
