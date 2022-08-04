cccbeaa66500242a799a2b531f27af86af8ea2b9
Marcos Paulo Santos Prado <marcospaulo_prado@live.com>
2022-08-03 23:35:19 -0300

    fix: remove foreignKey 'videoId'

--- api/models/categorias.js
+++ api/models/categorias.js
@@ -2,15 +2,8 @@
 const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
     class Categorias extends Model {
-        /**
-         * Helper method for defining associations.
-         * This method is not a part of Sequelize lifecycle.
-         * The `models/index` file will call this method automatically.
-         */
         static associate(models) {
-            Categorias.hasMany(models.Videos, {
-                foreignKey: 'videoId',
-            });
+            Categorias.hasMany(models.Videos);
         }
     }
     Categorias.init(
