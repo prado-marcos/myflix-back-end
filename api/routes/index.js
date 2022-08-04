105d38fd5bc0fd307ce058d70610e609abf86d69
Marcos Paulo Santos Prado <marcospaulo_prado@live.com>
2022-08-03 23:42:55 -0300

    refactor: export only the routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,6 @@
-const express = require('express');
 const videos = require('./videosRoutes.js');
 const categorias = require('./categoriasRoutes.js');
 
 module.exports = (app) => {
-    app.use(express.json(), videos, categorias);
+    app.use(videos, categorias);
 };
