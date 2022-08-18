beb4081f7a00b80d43738635dff63ddc78ec94cb
Marcos Paulo Santos Prado <marcospaulo_prado@live.com>
2022-08-18 11:34:02 -0300

    fix: routes priority

--- api/routes/videosRoutes.js
+++ api/routes/videosRoutes.js
@@ -3,9 +3,9 @@ const VideoController = require('../controllers/VideoController.js');
 const routes = Router();
 
 routes
-    .get('/videos', VideoController.listarVideos)
-    .get('/videos/:id', VideoController.acessarVideoPorId)
     .get('/videos/search', VideoController.acessarVideoPorTitulo)
+    .get('/videos/:id', VideoController.acessarVideoPorId)
+    .get('/videos', VideoController.listarVideos)
     .get('/categorias/:id/videos/', VideoController.listarVideosPorCategoria)
     .post('/videos', VideoController.cadastarVideo)
     .put('/videos/:id', VideoController.atualizarVideo)
